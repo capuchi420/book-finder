2a4803b1e2796bb392b6e02218639e7e96d1b762
Nemanja Stankovic <nemanjastan005@gmail.com>
2023-10-24 17:25:48 +0200

    Book pages connected to the db

--- public/app/src/pages/Book.js
+++ public/app/src/pages/Book.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar";
 import { Header } from "../components/Header";
 import { Form } from "../components/Form";
@@ -6,12 +6,25 @@ import { Cards } from "../components/Cards";
 import { Footer } from "../components/Footer";
 
 export const Book = () => {
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    const getABook = async () => {
+      const id = document.location.pathname.split('/')[2];
+      
+      fetch(`http://localhost:7777/db/getABook/${id}`).then(response => response.json()).then(data => setBook(data));
+    }
+
+    getABook();
+  }, []);
+
+  console.log(book)
+
   return(
     <>
       <Navbar />
-      <Header />
-      <Form />
-      <Cards/>
+      {book != null && <h1>{book.book_name}</h1>}
+      {book != null && <h3>{book.book_author}</h3>}
       <Footer />
     </>
   )
